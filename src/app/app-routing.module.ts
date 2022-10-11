e75d19ffe59e7f6f6fcc71dc3b462d50ab116f61
Efendi07 <asrulastaga@gmail.com>
2022-10-11 19:57:56 +0700

    backup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: "admin", component: AdminComponent},
+  {path: "login", component: LoginComponent},
+  {path: "", redirectTo:"login", pathMatch:"full" },
+  {path: "register", component: RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
